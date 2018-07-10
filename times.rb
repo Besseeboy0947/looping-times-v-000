7b0fb325fb4f55c4cbffe68777c63cc9ef61563b
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-07-10 03:36:09 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,7 +1,23 @@
 def using_times
-	#your code here
+puts "Wingardium Leviosa"
 end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  #spec
+#require_relative "spec_helper"
+#require_relative "../times.rb"
 
+#describe "#using_times" do 
+	#it "puts out the phrase 'Wingardium Leviosa' 7 times by using the `times` construct" do
 
-
-
+	#looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+	expect{ using_times }.to output(looping_string).to_stdout 
+	#end
+#end
